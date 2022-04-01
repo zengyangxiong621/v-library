477cfe4fe89fd243749f11c5c81b3dec782e52de
chendibo <chendibo@gridsum.com>
2022-04-01 18:22:51 +0800

    feat: 编辑数据源功能完善并联调完毕,优化添加/编辑数据源两块代码

--- src/routes/tempDataSource/tool/useFetch.ts
+++ src/routes/tempDataSource/tool/useFetch.ts
@@ -19,8 +19,8 @@ const catchErr = <T, U = Error>(
       return [err, null];
     });
 };
-const BASE_URL = "http://10.202.233.230:9572"; // 本地
-// const BASE_URL = "http://10.201.80.52:9572"; // 线上
+export const BASE_URL = "http://10.202.226.250:9572"; // 本地
+// export const BASE_URL = "http://10.201.80.52:9572"; // 线上
 const DEFAULT_OPTIONS = {
   method: "POST",
   mode: "cors",
@@ -29,23 +29,33 @@ const DEFAULT_OPTIONS = {
   },
 };
 
-
-export const useFetch = async(path: string, options: any):Promise<[Error | null, any]> => {
+/**
+ * description: 处理数据，对请求成功或者失败做统一处理
+ * params:  @path -- 请求路径
+ *          @options -- 优先级更高的自定义fetchAPI请求配置
+ * return: Promise<Pending>
+ */
+export const useFetch = async (
+  path: string,
+  options: any
+): Promise<[Error | null, any]> => {
   const finalPath = `${BASE_URL}${path}`;
   const finalParams = { ...DEFAULT_OPTIONS, ...options };
   // Fetch API 需要先转换一次格式
-  const finalFetch = fetch(finalPath, finalParams).then(response => response.json())
+  const finalFetch = fetch(finalPath, finalParams).then((response) =>
+    response.json()
+  );
 
   let [err, data] = await catchErr(finalFetch);
   /**** 根据返回数据进行统一的处理(小拦截器) *****/
-  if(err) {
-    message.error({content: '请求错误', duration: 2})
+  if (err) {
+    message.error({ content: "请求错误", duration: 2 });
     // 出错了需要终止程序的执行吗
     // throw Error('终止程序')
   }
-  if(data) {
+  if (data) {
     // data = data.data.content
-    data = data.data
+    data = data.data;
   }
   return [err, data];
 };
