7587277610706c650d04fbcdfe63691c7d4c1cf8
zhongguibin <zhongguibin@gridsum.com>
2022-05-11 18:13:39 +0800

    feat: 数据源设置逻辑

--- src/routes/dashboard/right/components/cusInput/index.jsx
+++ src/routes/dashboard/right/components/cusInput/index.jsx
@@ -1,6 +1,6 @@
 import React, { memo, useState, useEffect } from 'react';
 import './index.less'
-import { v4 as uuidv4 } from 'uuid';
+import debounce from 'lodash/debounce';
 
 import {
   Form,
@@ -14,17 +14,18 @@ const CusInput = props => {
   const [form] = Form.useForm();
   const _data = props.data
   const [value, setValue] = useState(_data.value)
-  const [key,setKey] = useState(uuidv4()) 
 
-  useEffect(()=>{
-    setKey(uuidv4())
-  },[_data.value])
-
-  const valueChange = (e) => {
+  const valueChange = debounce((e) => {
     setValue(e.target.value)
     _data.value = e.target.value
     props.onChange()
-  }
+  }, 300)
+
+  useEffect(()=>{
+    form.setFieldsValue({
+      [_data.name]: _data.value,
+    });
+  },[_data.value])
 
   return (
     <Form
@@ -35,7 +36,7 @@ const CusInput = props => {
     >
       <Form.Item label={_data.displayName}>
         <Form.Item  name={_data.name} noStyle>
-          <Input key={key} style={{ width: '100%' }} disabled={_data.disabled} className="cus-input" defaultValue={value} onChange={valueChange} />
+          <Input style={{ width: '100%' }} disabled={_data.disabled} className="cus-input" defaultValue={value} onChange={valueChange} />
         </Form.Item>
       </Form.Item>
     </Form>
