dfcc6ab6a942878ae48e10ee03c88d46d48a2bb0
尹伊 <yinyi@gridsum.com>
2022-06-23 14:54:58 +0800

    fix: 画布，组件，样式优化，交互部分接口对接

--- src/routes/dashboard/center/components/topBar/components/interaction/index.tsx
+++ src/routes/dashboard/center/components/topBar/components/interaction/index.tsx
@@ -1,51 +1,44 @@
-import { memo } from 'react'
+import { memo, useEffect, useState } from 'react'
 import './index.less'
 
 import EveryItem from '../everyItem/index'
 
-const dataArr = [
-  {
-    name: 'axaxaxax',
-  },
-  {
-    name: 'bbbbbbbbbbbbbbbbbbbbbbbbbbb',
-  },
-  {
-    name: 'c',
-  },
-  {
-    name: 'd',
-  },
-  {
-    name: 'e',
-  },
-  {
-    name: 'f',
-  },
-  {
-    name: 'g',
-  },
-  {
-    name: 'h',
-  },
-  {
-    name: 'i',
-  },
-  {
-    name: 'j',
-  },
-]
+import { http } from '@/services/request'
+
+// const dataArr = []
 
 const Interaction = (props: any) => {
-  // const { data } = props
+  const [dataArr, setDataArr] = useState<any>([])
+  useEffect(() => {
+    getData()
+  }, [])
+
+  // 获取地图组件数据
+  const getData = async () => {
+    const data: any = await http({
+      url: '/visual/module-manage/queryModuleList',
+      method: 'post',
+      body: {
+        type: ['interactive'],
+        status: 0,
+        pageNo: 0,
+        pageSize: 100
+      }
+    })
+    data.content.forEach((item: any) => {
+      item.photoPath = `${(window as any).CONFIG.COMP_URL}${item.photoPath}`
+    })
+    setDataArr(data.content)
+  }
   return (
     <div className='Interaction-wrap'>
       {
+        dataArr.length ? 
         dataArr.map((item: any, index: number) => {
           return (
             <EveryItem key={index} data={item} />
           )
-        })
+        }): <div className='Interaction-wrap'>暂无数据</div>
       }
     </div>
   )
