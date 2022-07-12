4b374d7f83dfe0dd4bc314542ae725b4ba7ddde2
曾扬雄 <zengyangxiong@gridsum.com>
2022-07-12 11:51:30 +0800

    Update index.tsx

--- src/routes/dashboard/center/components/CustomDraggable/index.tsx
+++ src/routes/dashboard/center/components/CustomDraggable/index.tsx
@@ -687,7 +687,7 @@ const CustomDraggable
                         </div>
                       </>
                   }
-                  {/*<div style={{ position: 'absolute', left: 0, top: 0, bottom: 0, right: 0 }} />*/}
+                  <div style={{ position: 'absolute', left: 0, top: 0, bottom: 0, right: 0 }} />
                   {/*增加一个类似透明蒙版的div，防止 echarts 图表误触、img 标签拖拽问题*/}
                   <div className="component-border">
                     <span
