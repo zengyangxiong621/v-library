2562360577db3ed1404c17fbc14413d822675239
尹伊 <yinyi@gridsum.com>
2022-07-13 09:57:23 +0800

    fix: 画布编辑页面样式增加禁止鼠标样式

--- src/routes/dashboard/center/components/CustomDraggable/index.tsx
+++ src/routes/dashboard/center/components/CustomDraggable/index.tsx
@@ -634,7 +634,7 @@ const CustomDraggable
                           : ''
                         }
                       </div> : <>
-                        <div data-id={layer.id} style={{ width: '100%', height: '100%' }}>
+                        <div data-id={layer.id} style={{ width: '100%', height: '100%', pointerEvents: 'none' }}>
                           {
                             // layer.moduleName === 'text' ? <Text componentConfig={component}/> :
                             //   <CompImage componentConfig={component}/>
