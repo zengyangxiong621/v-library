fd278d6e5272186b9361a4d41502507a60898562
曾扬雄 <zengyangxiong@gridsum.com>
2022-08-08 14:31:27 +0800

    fix: 先注释掉滚动选择器、选项卡

--- src/routes/dashboard/center/components/CustomDraggable/index.tsx
+++ src/routes/dashboard/center/components/CustomDraggable/index.tsx
@@ -975,15 +975,15 @@ const CustomDraggable
                                 comData={ getComDataWithFilters(bar.componentData, component, bar.componentFilters, bar.dataContainerDataList, bar.dataContainerList, bar.callbackArgs) }
                               >
                               </ScrollTable> :
-                              layer.moduleName === 'tab' ?
+/*                              layer.moduleName === 'tab' ?
                                 <Tab
                                   onChange={(val:any)=>handleValueChange(val, component, layer.id)}
                                   componentConfig={ component }
                                   fields={ getFields(component) }
                                   comData={ getComDataWithFilters(bar.componentData, component, bar.componentFilters, bar.dataContainerDataList, bar.dataContainerList, bar.callbackArgs) }
                                 >
-                                </Tab> :
-                                layer.moduleName === 'scrollSelect' ?
+                                </Tab> :*/
+/*                                layer.moduleName === 'scrollSelect' ?
                                   <ScrollSelect
                                     onChange={(val:any)=>handleValueChange(val, component, layer.id)}
                                     componentConfig={ component }
@@ -998,7 +998,7 @@ const CustomDraggable
                                   fields={ getFields(component) }
                                   comData={ getComDataWithFilters(bar.componentData, component, bar.componentFilters, bar.dataContainerDataList, bar.dataContainerList, bar.callbackArgs) }
                                 >
-                                </TimeSelect> :
+                                </TimeSelect> :*/
                                 layer.moduleName === 'worldMap' ?
                                   <WorldMap
                                     onChange={(val:any)=>handleValueChange(val, component, layer.id)}
