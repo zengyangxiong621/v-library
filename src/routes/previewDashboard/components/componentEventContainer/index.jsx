6551ebab42bac0c53a684a656ac3121e72ee7f93
彭向明 <pengxiangming@gridsum.com>
2022-08-29 18:25:34 +0800

    fix:常规表格预览失败问题

--- src/routes/previewDashboard/components/componentEventContainer/index.jsx
+++ src/routes/previewDashboard/components/componentEventContainer/index.jsx
@@ -14,6 +14,7 @@ import RankingBar from '@/customComponents/echarts/components/rankingBar/v1.1.0'
 import Tab from '@/customComponents/interactive/tab'
 import ScrollSelect from '@/customComponents/interactive/scrollSelect/index'
 import Counter from  '@/customComponents/assist/counter2'
+import NormalTable from '@/customComponents/table/normalTable'
 import {connect} from "dva"
 
 // import './index.css'
@@ -501,6 +502,10 @@ const ComponentEventContainer = ({previewDashboard, dispatch, events = [], id = 
             {...props}
           >
           </Timeline>
+                :props.componentConfig.moduleName==='normalTable'?
+          <NormalTable
+            {...props}
+          ></NormalTable>
           // : props.componentConfig.moduleName === 'CardFlipper_1' ?
           // <CardFlipper1
           //   scale={scale}
