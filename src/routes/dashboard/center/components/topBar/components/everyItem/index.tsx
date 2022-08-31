96f2dc8bbfbf4e178a8d99b796818d70d0490b92
尹伊 <yinyi@gridsum.com>
2022-08-31 11:23:16 +0800

    fix: 素材无法添加问题处理

--- src/routes/dashboard/center/components/topBar/components/everyItem/index.tsx
+++ src/routes/dashboard/center/components/topBar/components/everyItem/index.tsx
@@ -5,17 +5,35 @@ import './index.less'
 
 const EveryItem = (props: any) => {
   const { data, dispatch, bar } = props
-  const componentCreate = () => {
-    const { moduleDefaultConfig } = bar
-    const currentDefaultConfig = moduleDefaultConfig.find((item: any) => {
-      return item.moduleName === data.moduleName
-    })
-    dispatch({
-      type: 'bar/createComponent',
-      payload: currentDefaultConfig,
-      itemData: data
-    })
+
+  const importComponent = (data: any) => {
+    return axios.get(`${(window as any).CONFIG.COMP_URL}/${data.moduleType}/${data.moduleName}/${data.moduleVersion}/${data.moduleName}.js`).then(res => res.data);
   }
+
+  const componentCreate = async() => {
+    const { moduleType } = data
+    if(moduleType === "design"){
+      window.eval(`${await importComponent(data)}`)
+      const { ComponentDefaultConfig } = (window as any).VComponents;
+      dispatch({
+        type: 'bar/createComponent',
+        payload: ComponentDefaultConfig,
+        itemData: data
+      })
+    }else{
+      const { moduleDefaultConfig } = bar
+      const currentDefaultConfig = moduleDefaultConfig.find((item: any) => {
+        return item.moduleName === data.moduleName
+      })
+      dispatch({
+        type: 'bar/createComponent',
+        payload: currentDefaultConfig,
+        itemData: data
+      })
+    }
+  }
+
+  
 
   return (
     <div className='EveryItem-wrap' onClickCapture={componentCreate}>
