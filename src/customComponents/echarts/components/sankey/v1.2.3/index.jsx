cade979899e8944c9659b816c38735d9da77cf78
林宾宾 <linbinbin@gridsum.com>
2022-09-21 14:06:37 +0800

    feat: 更新时间器组件:添加主题配置

--- src/customComponents/echarts/components/sankey/v1.2.3/index.jsx
+++ src/customComponents/echarts/components/sankey/v1.2.3/index.jsx
@@ -40,7 +40,6 @@ const RingRatio = (props) => {
   // 组件静态或者传入组件的数据
   const originData = props.comData || data
   const componentThemeConfig = props.themeConfig
-  console.log('------------------------',componentThemeConfig)
   // originData中有多项数据，只取第一项
   const firstData = originData[0]
   const nodes = deepClone(firstData[finalFieldsArr[0]])
