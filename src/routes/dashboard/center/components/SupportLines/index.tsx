5499db45e57ad0bbeee9613a9e53ef475185a950
chendibo <chendibo@gridsum.com>
2022-10-12 15:03:35 +0800

    fix: 组件更新放开对name===customColumn的拦截

--- src/routes/dashboard/center/components/SupportLines/index.tsx
+++ src/routes/dashboard/center/components/SupportLines/index.tsx
@@ -12,9 +12,6 @@ const SupportLines = ({ bar, cRef }: SupportLineProperty) => {
   useImperativeHandle(cRef, () => ({
     // changeVal 就是暴露给父组件的方法
     handleSetPosition: (left: number, top: number, display = "block") => {
-      console.log('这里没有说法吗')
-      console.log('left', left)
-      console.log('top', top)
       setStyle({ left, top, display });
       lineRef.current.style.left = left + 'px'
       lineRef.current.style.top = top + 'px'
