e56f1ddf46c62a048387b767db0d0e41dc5a56d9
厉宗梅 <lizongmei@gridsum.com>
2022-10-22 09:03:00 +0800

    去掉次级域名的配置

--- src/index.tsx
+++ src/index.tsx
@@ -30,7 +30,7 @@ const init = () => {
   // -> 初始化路由
   app.router(({ app }: any) => (
     <div>
-      <BrowserRouter basename="/idv">{createRoutes(app, RoutesConfig)}</BrowserRouter>
+      <BrowserRouter basename="/">{createRoutes(app, RoutesConfig)}</BrowserRouter>
     </div>
   ));
   // -> Start
