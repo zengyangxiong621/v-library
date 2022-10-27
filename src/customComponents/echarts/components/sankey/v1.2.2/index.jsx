91f4d7b10e7eb4135f4d6dbbbb25be33595a3f17
庄志霖 <zhuangzhilin@gridsum.com>
2022-10-27 16:42:03 +0800

    fix: eslint校验

--- src/customComponents/echarts/components/sankey/v1.2.2/index.jsx
+++ src/customComponents/echarts/components/sankey/v1.2.2/index.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import EC from '../../EC'
+import React from "react";
+import EC from "../../EC";
 
-import * as echarts from 'echarts';
-import ComponentDefaultConfig from './config'
+import * as echarts from "echarts";
+import ComponentDefaultConfig from "./config";
 
 // 深拷贝
-const deepClone =  (obj) =>  {
-  if (obj === null || typeof obj !== 'object') {
-    return obj
+const deepClone = (obj) => {
+  if (obj === null || typeof obj !== "object") {
+    return obj;
   }
-  const result = Array.isArray(obj) ? [] : {}
+  const result = Array.isArray(obj) ? [] : {};
   if (Array.isArray(obj)) {
     for (let i = 0; i < obj.length; i++) {
-      result[i] = typeof obj[i] === 'object' ? deepClone(obj[i]) : obj[i]
+      result[i] = typeof obj[i] === "object" ? deepClone(obj[i]) : obj[i];
     }
-  } else if (typeof obj === 'object') {
+  } else if (typeof obj === "object") {
     Object.keys(obj).forEach((k) => {
-      result[k] = typeof obj[k] === 'object' ? deepClone(obj[k]) : obj[k]
-    })
+      result[k] = typeof obj[k] === "object" ? deepClone(obj[k]) : obj[k];
+    });
   }
-  return result
-}
+  return result;
+};
 
 function rgb() {
   var r = Math.floor(Math.random() * 256);
@@ -32,83 +32,84 @@ function rgb() {
 
 
 const RingRatio = (props) => {
-  const componentConfig = props.componentConfig || ComponentDefaultConfig
-  const { config } = componentConfig
-  const { data } = componentConfig.staticData
+  const componentConfig = props.componentConfig || ComponentDefaultConfig;
+  const { config } = componentConfig;
+  const { data } = componentConfig.staticData;
   // 最新字段
-  const finalFieldsArr = props.fields || ['nodes','links']
+  const finalFieldsArr = props.fields || ["nodes", "links"];
   // 组件静态或者传入组件的数据
-  const originData = props.comData || data
+  const originData = props.comData || data;
   // originData中有多项数据，只取第一项
-  const firstData = originData[0]
-  const nodes = deepClone(firstData[finalFieldsArr[0]])
-  const links = deepClone(firstData[finalFieldsArr[1]])
+  const firstData = originData[0];
+  const nodes = deepClone(firstData[finalFieldsArr[0]]);
+  const links = deepClone(firstData[finalFieldsArr[1]]);
 
   // 获取config中的配置
   const getTargetConfig = (Arr) => {
-    let targetConfig = {}
+    let targetConfig = {};
     Arr.forEach((item) => {
-      let { name, value, options, flag, displayName } = item
-      if (item.hasOwnProperty('value')) {
+      let { name, value, options, flag, displayName } = item;
+      // if (item.hasOwnProperty("value")) {
+      if (Object.prototype.hasOwnProperty.call(item, "value")) {
         // 对 系列一栏 做特殊处理
-        if (flag === 'specialItem') {
-          name = displayName
+        if (flag === "specialItem") {
+          name = displayName;
         }
         if (Array.isArray(value)) {
-          targetConfig[name] = getTargetConfig(value)
+          targetConfig[name] = getTargetConfig(value);
         } else {
-          targetConfig[name] = value
+          targetConfig[name] = value;
         }
       } else if (Array.isArray(options) && options.length) {
-        targetConfig[name] = getTargetConfig(options)
+        targetConfig[name] = getTargetConfig(options);
       }
-    })
-    return targetConfig
-  }
+    });
+    return targetConfig;
+  };
   // console.log(getTargetConfig(config));
-  const {globalStyle,margin,label,linksLineStyle,tooltip,dataSeries} = getTargetConfig(config)
-  const {nodeWidth,nodeGap,emphasis,draggable} = globalStyle
-  const {left,top,right,bottom} = margin
-  const {labelTextStyle,numberLabel,offset:{x,y},position} = label
-  const {color,opacity,curveness,customColor:{customColorShow,styleColor}} = linksLineStyle
-  const {backgroundColor,tooltipTextStyle} = tooltip
+  const { globalStyle, margin, label, linksLineStyle, tooltip, dataSeries } = getTargetConfig(config);
+  const { nodeWidth, nodeGap, emphasis, draggable } = globalStyle;
+  const { left, top, right, bottom } = margin;
+  const { labelTextStyle, numberLabel, offset: { x, y }, position } = label;
+  const { color, opacity, curveness, customColor: { customColorShow, styleColor } } = linksLineStyle;
+  const { backgroundColor, tooltipTextStyle } = tooltip;
 
-  const dataSeriesValues = Object.values(dataSeries)
-  const dataSeriesObj = {}
+  const dataSeriesValues = Object.values(dataSeries);
+  const dataSeriesObj = {};
   dataSeriesValues.forEach(item => {
-    dataSeriesObj[item.fieldName] = item.barColor
-  })
+    dataSeriesObj[item.fieldName] = item.barColor;
+  });
 
   links.forEach(item => {
     item.lineStyle = {
       color: !customColorShow ? color : styleColor, // target 或 自定义
-      opacity: '0.1', // 0.1
-      curveness: '0.5'// 0-1
-    }
-  })
-  const allSource = links.map(item => item.source)
-  const surce =  [...new Set(allSource)]
+      opacity: "0.1", // 0.1
+      curveness: "0.5"// 0-1
+    };
+  });
+  const allSource = links.map(item => item.source);
+  const surce = [...new Set(allSource)];
   nodes.forEach(item => {
-    if(surce.includes(item.name)){
+    if (surce.includes(item.name)) {
       item.label = {
         position: "right"
-      }
+      };
     }
     item.itemStyle = {
       color: dataSeriesObj[item.name] || rgb()
-    }
-  })
+    };
+  });
 
 
 
   // 环形宽度
   const getOption = () => ({
     tooltip: {
-      trigger: 'item',
-      triggerOn: 'mousemove',
+      trigger: "item",
+      triggerOn: "mousemove",
     },
     series: {
-      type: 'sankey',
+      type: "sankey",
       nodeWidth,
       nodeGap,
       draggable,
@@ -117,7 +118,7 @@ const RingRatio = (props) => {
       right,
       bottom,
       emphasis: {
-        focus: emphasis ? 'adjacency' : 'none',
+        focus: emphasis ? "adjacency" : "none",
       },
       tooltip: {
         backgroundColor,
@@ -131,7 +132,7 @@ const RingRatio = (props) => {
       label: {
         show: true,
         position,
-        offset: [x,y],
+        offset: [x, y],
         color: labelTextStyle.color,
         fontFamily: labelTextStyle.fontFamily,
         fontSize: labelTextStyle.fontSize,
@@ -141,28 +142,28 @@ const RingRatio = (props) => {
       links
 
     }
-  })
+  });
 
   const onChartReady = echarts => {
   };
   const onChartClick = (param, echarts) => {
-  }
+  };
   let onEvents = {
     click: onChartClick
-  }
+  };
   return (
     <EC
       option={getOption()}
       onChartReady={onChartReady}
       onEvents={onEvents}
     />
-  )
+  );
 
-}
+};
 
 export {
   RingRatio,
   ComponentDefaultConfig
-}
+};
 
-export default RingRatio
+export default RingRatio;
