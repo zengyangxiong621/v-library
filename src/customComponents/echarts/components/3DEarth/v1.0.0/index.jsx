7466db8c1ece6d56bf3c8df75450b6c2657db79f
尹伊 <yinyi@gridsum.com>
2022-11-08 15:11:20 +0800

    fix: 3d地球样式优化

--- src/customComponents/echarts/components/3DEarth/v1.0.0/index.jsx
+++ src/customComponents/echarts/components/3DEarth/v1.0.0/index.jsx
@@ -1,41 +1,45 @@
-import ComponentDefaultConfig from './config'
-import * as echarts from 'echarts'
-import EC from '../../EC'
-import React from 'react'
-import 'echarts-gl';
-import earth from './earth.jpg'
-import bathymetry from './bathymetry.jpg';
-import starfield from './starfield.jpg'
+import ComponentDefaultConfig from "./config";
+import * as echarts from "echarts";
+import EC from "../../EC";
+import React from "react";
+import "echarts-gl";
+import earth from "./earth.jpg";
+import bathymetry from "./bathymetry.jpg";
+import starfield from "./starfield.jpg";
 
 const Earth = (props) => {
-
   const getOption = () => ({
     globe: {
       baseTexture: earth, // 地图图片
       heightTexture: bathymetry, //地球顶点的置换纹理
       // environment: starfield, // 环境贴图
-      shading: 'color', //地球中三维图形的着色效果 color lambert realistic
-      globeRadius:100, // 地球的半径
-      displacementScale:0.03, //地球顶点位移的大小。默认为 0 地表高度细节
+      shading: "color", //地球中三维图形的着色效果 color lambert realistic
+      globeRadius: 100, // 地球的半径
+      displacementScale: 0.03, //地球顶点位移的大小。默认为 0 地表高度细节
       viewControl: {
-        autoRotate: true
+        autoRotate: true,
       },
       atmosphere: {
-        show: true
+        show: true,
       },
     },
-  })
+  });
 
-  const onChartClick = (param, echarts) => { }
-  const onChartReady = (echarts) => { }
+  const onChartClick = (param, echarts) => {};
+  const onChartReady = (echarts) => {};
   let onEvents = {
     click: onChartClick,
-  }
+  };
   return (
-    <EC option={getOption()} onChartReady={onChartReady} onEvents={onEvents} />
+    <EC
+      option={getOption()}
+      onChartReady={onChartReady}
+      onEvents={onEvents}
+      opts={{ renderer: "canvas" }}
+    />
     // <div>123</div>
-  )
-}
+  );
+};
 
-export { Earth, ComponentDefaultConfig }
-export default Earth
+export { Earth, ComponentDefaultConfig };
+export default Earth;
