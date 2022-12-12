93b0e4fe5d9836708a0028c3e2528c2f640509cc
zengyangxiong <zengyangxiong@gridsum.com>
2022-12-12 14:44:34 +0800

    fix: dashboard页不设置动态路由加载

--- src/routes/myDashboard/index.tsx
+++ src/routes/myDashboard/index.tsx
@@ -1,13 +1,15 @@
 import { newDynamic, createRoute } from "../../utils/core";
-
-const routesConfig = (app: any) => ({
-  path: "/dashboard-manage",
-  title: "我的可视化",
-  component: newDynamic(
-    app,
-    [import("../../models/dashboardManage"), import("../../models/bar")],
-    () => import("./myDashboard")
-  ),
-});
+import myDashboard from "./myDashboard";
+const routesConfig = (app: any) => {
+  return {
+    path: "/dashboard-manage",
+    title: "我的可视化",
+    component: newDynamic(
+      app,
+      [import("../../models/dashboardManage"), import("../../models/bar")],
+      () => import("./myDashboard")
+    ),
+  };
+};
 const Page = (app: any) => createRoute(app, routesConfig);
 export default Page;
