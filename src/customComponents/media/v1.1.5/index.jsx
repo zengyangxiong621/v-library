36d4a1b347d713f0cd5a583a692c32cf8f91efb7
尹伊 <yinyi@gridsum.com>
2022-12-16 17:51:15 +0800

    fix: 音视频组件版本升级，权限管理模块样式优化

---
+++ src/customComponents/media/v1.1.5/index.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+import ComponentDefaultConfig from "./config";
+import { CaretRightOutlined } from "@ant-design/icons";
+import "./index.less";
+
+const Media = (props) => {
+  const componentConfig = props.componentConfig || ComponentDefaultConfig;
+  const { config, staticData } = componentConfig;
+  const componentData = props.comData || staticData.data;  // 过滤后的数据
+  const _fields = props.fields;
+  const [MediaAutoPlaying, setMediaAutoPlaying] = useState(false);
+  const getStyle = (config) => {
+    const style = {};
+    if (Array.isArray(config)) {
+      config.forEach(item => {
+        if (Array.isArray(item.value)) {
+          style[item.name] = getStyle(item.value);
+        } else if (item.options && Array.isArray(item.options)) {
+          item.options.forEach(item => {
+            style[item.key] = getStyle(item.value);
+          });
+        } else {
+          style[item.name] = item.value;
+        }
+      });
+    }
+    return style;
+  };
+  const style = getStyle(config);
+  const { dimension: { width, height }, Loop, Muted, controls, hideDefault, autoPlaying, mediaFile } = style;
+  const getMediaUrl = () => {
+    if (mediaFile) {
+      return mediaFile;
+    }
+    return componentData[0][_fields[0]];
+  };
+
+  useEffect(() => {
+    props.onDataChange && props.onDataChange(componentData);
+  }, [componentData]);
+
+  useEffect(() => {
+    autoPlaying && setMediaAutoPlaying(true);
+  }, [autoPlaying]);
+
+  const PlayBtn = () => {
+    const handleToPlay = () => {
+      setMediaAutoPlaying(true);
+    };
+    return (
+      <div className='playBtn' onClick={handleToPlay}>
+        <CaretRightOutlined />
+      </div>
+    );
+  };
+
+  const handleClick = (e) => {
+    props.onClick && props.onClick(e, props.comData);
+  };
+  const handleMouseEnter = (e) => {
+    props.onMouseEnter && props.onMouseEnter(e, props.comData);
+  };
+  const handleMouseLeave = (e) => {
+    props.onMouseEnter && props.onMouseLeave(e, props.comData);
+  };
+
+  return hideDefault ? (<></>) : (
+    <div 
+      className='videoContainer'
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+    >
+      <ReactPlayer
+        playing={MediaAutoPlaying}
+        width={`${width}px`}
+        height={`${height}px`}
+        loop={Loop}
+        muted={Muted}
+        controls={controls}
+        url={getMediaUrl()}
+      />
+      {
+        !(autoPlaying || controls) && !MediaAutoPlaying ? (<PlayBtn></PlayBtn>) : null
+      }
+    </div>
+  );
+};
+export {
+  ComponentDefaultConfig,
+  Media
+};
+export default Media;
